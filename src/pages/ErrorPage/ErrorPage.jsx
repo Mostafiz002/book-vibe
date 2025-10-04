1e7664b0adab1184d99237a2bfbf3832efcb80ba
Mostafiz002 <mo.mahin4@gmail.com>
2025-10-04 17:26:30 +0600

    first commit

---
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ErrorPage = () => {
+  return (
+    <div className="h-screen w-screen flex items-center justify-center">
+      <h1 className="text-2xl font-medium">404 Page Not Found</h1>
+    </div>
+  );
+};
+
+export default ErrorPage;
